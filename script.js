f95547f2c89f280c87466409f36b88d5a2431252
Aniruddh <ojhaa94@gmail.com>
2021-03-16 12:09:36 +0000

    refactors error message

--- script.js
+++ script.js
@@ -26,23 +26,43 @@ function isValidEmail(email) {
 	return re.test(String(email).toLowerCase())
 }
 
+// Check Required Fields
+function checkRequired(inputArray) {
+	inputArray.forEach(function(input) {
+		if(input.value.trim() === '') {
+			showError(input, `${getFieldName(input)} is required`);
+		} else {
+			showSuccess(input);
+		}
+	});
+}
+
+// Check Length
+function checkLength(input, min, max) {
+	if(input.value.length < min) {
+		showError(input, `${getFieldName(input)} must be at least ${min} characters`);
+	} else if (input.value.length > max) {
+		showError(input, `${getFieldName(input)} must be less than ${max} characters`);
+	} else {
+		showSuccess(input);
+	}
+}
+
+// Get Field Name
+function getFieldName(input) {
+	return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+	// capitalizes just the first letter of the word with .charAt(0).toUpperCase()
+	// slices off the first letter of the word with slice(1)
+	// concatenates them together
+}
+
 //Event Listeners
 form.addEventListener('submit', function(e) {
 	e.preventDefault();
 	
-	if(username.value === '') {
-		showError(username, 'Username is required')
-	} else {
-		showSuccess(username);
-	}
-
-	if(email.value === '') {
-		showError(email, 'Email is required')
-	} else if (!isValidEmail(email.value)) {
-		showError(email, 'Email is invalid')
-	} else {
-		showSuccess(email);
-	}
-
+	checkRequired([username, email, password, password2]);
+	checkLength(username, 3, 15);
+	checkLength(password, 6, 25);
 	
 })
+
